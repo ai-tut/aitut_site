caceec9984b780430873df1d74756a0e21818f85
lpm <lpmathiasen@icloud.com>
2025-08-10 19:36:00 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
